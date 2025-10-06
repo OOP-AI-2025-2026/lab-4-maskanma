7970e7d9e5b61566844b7110fdfdf05558a02452
maskanma <9118729@stud.op.edu.ua>
2025-10-06 15:57:18 +0300

    Update Task2Test.java

--- src/test/java/Task2Test.java
+++ src/test/java/Task2Test.java
@@ -1,8 +1,9 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
-import ua.opnu.java.inheritance.account.Credit;
-import ua.opnu.java.inheritance.account.Debit;
-import ua.opnu.java.inheritance.account.Startup;
+import ua.opnu.account.Credit;
+import ua.opnu.account.Debit;
+import ua.opnu.account.MinMaxAccount;
+import ua.opnu.account.Startup;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
