bf5af988a3a4a2ca129abbd99c65477284989638
maskanma <9118729@stud.op.edu.ua>
2025-10-06 15:57:00 +0300

    Update Task1Test.java

--- src/test/java/Task1Test.java
+++ src/test/java/Task1Test.java
@@ -1,7 +1,10 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
-import ua.opnu.java.inheritance.bill.Employee;
-import ua.opnu.java.inheritance.bill.Item;
+import ua.opnu.bill.DiscountBill;
+import ua.opnu.bill.Employee;
+import ua.opnu.bill.Item;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
